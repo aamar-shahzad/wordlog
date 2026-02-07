45953879c14a4877682eced0588088b488896138
Aamar Shahzad <51914039+aamar-shahzad@users.noreply.github.com>
2026-02-06 20:55:05 -0500

    initial commit

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "wordlog-v1";
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(["./", "./index.html"])),
+  );
+});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request)),
+  );
+});
